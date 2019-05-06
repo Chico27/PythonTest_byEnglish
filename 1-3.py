fd9cfb4c2e5144ff3a581ec995a9ec3739874178
Chico27 <masamasa.987654321@gmail.com>
2019-05-06 16:48:26 +0900

    first commit

---
+++ 1-3.py
@@ -0,0 +1,11 @@
+
+character_name = "John"
+character_age = "35"
+is_male = True
+
+print("There aonce was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+
+character_name = "Mike"
+print("He really liked th name " + character_name + ", ")
+print("but didn't like being  " + character_age + "")
