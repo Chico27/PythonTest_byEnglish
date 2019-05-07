9ceec52970e6fd4f90be0d7e529071dd97c21c4a
Chico27 <masamasa.987654321@gmail.com>
2019-05-07 22:30:22 +0900

    new files

---
+++ 1-12.py
@@ -0,0 +1,16 @@
+
+def sayhi():
+    print("Hello User")
+
+
+print("Top")
+sayhi()
+print("Bottom")
+
+
+def say_hi(name, age):
+    print("Hello " + name, "you are " + str(age))
+
+
+say_hi("Mike", 35)
+say_hi("Steve", 70)
