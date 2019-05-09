6c5dc1614a963870d9c806933c15cb40c6837a60
Chico27 <masamasa.987654321@gmail.com>
2019-05-09 22:26:46 +0900

    new files

---
+++ 1-17.py
@@ -0,0 +1,19 @@
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "Jun": "June",
+}
+
+print(monthConversions["Mar"])
+print(monthConversions["Jan"])
+print(monthConversions.get("Luv"))
+print(monthConversions.get("Luv", "Not a valid Key"))
+
+
+
+
+
+
---
+++ 1-24.py
@@ -0,0 +1,16 @@
+
+'''
+wiohew
+te4
+t4e
+te4t
+'''
+
+
+# This prints out a string
+# next comment
+print("Comments are fun")
+
+
+# print("Comments are fun")
+
